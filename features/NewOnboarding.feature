235bac92fec8c3b9ce64dd86296c27231cf7b945
andreluiz-grafeno <andre.luiz@grafeno.digital>
2022-06-21 11:53:11 -0300

    Teste implementado para envio de um novo Onboarding

--- features/NewOnboarding.feature
+++ features/NewOnboarding.feature
@@ -6,9 +6,13 @@ Estando na página inicial da pagamentos
 Preciso clicar em novo pedido de Onboarging
 
 @onboarding
-Cenario: Acessar formulario de novo Onboarding
+Cenario:Preencher formulario de novo Onboarding
 
 Dado que eu esteja na página inicial da pagamentos 'https://dev-pagamentos.grafeno.be/admin'
 E realize o login com o usuário 'andre.luiz@grafeno.digital' e senha 'GR@feno10'
 Quando clicar em novo pedido de Onboarding
 Então deve ser exibido o formulário de novo Onboarding
+E deve ser preenchido a Fantasia 'AUTOMAÇÃO FANTASIA', Razão Social 'AUTOMAÇÃO RAZÃO SOCIAL', CNPJ '08987286000152', Email 'automation@grafeno.digital'
+Quando clicar em salvar
+Então deverá ser exibida a mensagem 'Onboarding criado com sucesso'
+
--- features/step_definitions/NewOnboarding_steps.rb
+++ features/step_definitions/NewOnboarding_steps.rb
@@ -2,7 +2,7 @@ Dado('que eu esteja na página inicial da pagamentos {string}') do |plataforma|
     visit plataforma
   end
   
-  Dado('realize o login com o usuário {string} e senha {string}') do |usuario, senha|
+  E('realize o login com o usuário {string} e senha {string}') do |usuario, senha|
     find('#user_email').set usuario
     find('#user_password').set senha
     find('input[name="commit"]').click
@@ -16,4 +16,24 @@ Dado('que eu esteja na página inicial da pagamentos {string}') do |plataforma|
   
   Então('deve ser exibido o formulário de novo Onboarding') do
     expect(page).to have_content 'Novo(a) Participante'
+  end
+
+
+
+  E('deve ser preenchido a Fantasia {string}, Razão Social {string}, CNPJ {string}, Email {string}') do |fantasia, razao_social, cnpj, email|
+    find('#client_name').set fantasia
+    find('#client_company_name').set razao_social
+    find('#client_document_number').set cnpj
+    find('#client_onboarding_email').set email
+    find('.select2-selection__placeholder').click
+    sleep 2
+    all('.select2-results__option').sample.click
+  end
+
+  Quando('clicar em salvar') do
+    find('input[type="submit"]').click
+  end
+
+  Então('deverá ser exibida a mensagem {string}') do |mensagem_sucesso|
+    expect(page).to have_content mensagem_sucesso
   end
